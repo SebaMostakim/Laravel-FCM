c77faad72bdd2e756a67446caa0ae630195efce9
mostakim rahman <mostakim.rahman@iamsheba.xyz>
2024-12-30 11:18:33 +0600

    laravel-fcm upgraded for laravel 10

--- src/FCMServiceProvider.php
+++ src/FCMServiceProvider.php
@@ -2,53 +2,81 @@
 
 namespace LaravelFCM;
 
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Str;
 use LaravelFCM\Sender\FCMGroup;
 use LaravelFCM\Sender\FCMSender;
 use Illuminate\Support\ServiceProvider;
 
 class FCMServiceProvider extends ServiceProvider
 {
-    protected $defer = true;
-
-    public function boot()
-    {
-        if (Str::contains($this->app->version(), 'Lumen')) {
-            $this->app->configure('fcm');
-        } else {
-            $this->publishes([
-                __DIR__.'/../config/fcm.php' => config_path('fcm.php'),
-            ]);
-        }
-    }
-
+    /**
+     * Register services in the container.
+     *
+     * @return void
+     */
     public function register()
     {
-		if (!Str::contains($this->app->version(), 'Lumen')) {
-            $this->mergeConfigFrom(__DIR__.'/../config/fcm.php', 'fcm');
+        // Merge configuration for non-Lumen applications
+        if (!$this->isLumen()) {
+            $this->mergeConfigFrom(__DIR__ . '/../config/fcm.php', 'fcm');
         }
 
+        // Register singleton for FCM client
         $this->app->singleton('fcm.client', function ($app) {
             return (new FCMManager($app))->driver();
         });
 
+        // Register FCM group binding
         $this->app->bind('fcm.group', function ($app) {
-            $client = $app[ 'fcm.client' ];
-            $url = $app[ 'config' ]->get('fcm.http.server_group_url');
+            $client = $app['fcm.client'];
+            $url = $app['config']->get('fcm.http.server_group_url');
 
             return new FCMGroup($client, $url);
         });
 
+        // Register FCM sender binding
         $this->app->bind('fcm.sender', function ($app) {
-            $client = $app[ 'fcm.client' ];
-            $url = $app[ 'config' ]->get('fcm.http.server_send_url');
+            $client = $app['fcm.client'];
+            $url = $app['config']->get('fcm.http.server_send_url');
 
             return new FCMSender($client, $url);
         });
     }
 
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->isLumen()) {
+            $this->app->configure('fcm');
+        } else {
+            // Publish configuration file for Laravel applications
+            $this->publishes([
+                __DIR__ . '/../config/fcm.php' => config_path('fcm.php'),
+            ], 'fcm-config');
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
     public function provides()
     {
         return ['fcm.client', 'fcm.group', 'fcm.sender'];
+    }
+
+    /**
+     * Determine if the application is running under Lumen.
+     *
+     * @return bool
+     */
+    protected function isLumen()
+    {
+        return str_contains($this->app->version(), 'Lumen');
     }
 }
